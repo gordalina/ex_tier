6021ade18b4eacbfcb784fe11a9e9e9d644a0072
Samuel Gordalina <samuel.gordalina@gmail.com>
2023-05-03 14:41:57 -1000

    Refactor client testing

--- test/client/client_test.exs
+++ test/client/client_test.exs
@@ -1,27 +1,9 @@
-defmodule ExTier.Client.ResponseMiddlewareTest do
+defmodule ExTier.ClientTest do
   use ExUnit.Case, async: false
 
   import ExUnit.CaptureLog
 
   alias ExTier.Client
-
-  describe "with test clock" do
-    setup do
-      Application.put_env(:ex_tier, :clock_id, nil)
-      on_exit(fn -> Application.put_env(:ex_tier, :clock_id, nil) end)
-    end
-
-    test "enabled" do
-      Application.put_env(:ex_tier, :clock_id, "test_clock")
-      Tesla.Mock.mock(fn env -> %{env | status: 200, body: env.headers} end)
-      assert {:ok, [{"tier-clock", "test_clock"}]} == Client.get("/")
-    end
-
-    test "disabled" do
-      Tesla.Mock.mock(fn env -> %{env | status: 200, body: env.headers} end)
-      assert {:ok, []} == Client.get("/")
-    end
-  end
 
   describe "tier error" do
     setup do
