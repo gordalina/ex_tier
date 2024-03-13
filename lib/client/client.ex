193e0d386f996d5a893093a50da62607c239d461
Samuel Gordalina <samuel.gordalina@gmail.com>
2024-03-12 15:40:01 -1000

    Add retries

--- lib/client/client.ex
+++ lib/client/client.ex
@@ -16,6 +16,8 @@ defmodule ExTier.Client do
   plug(ExTier.Client.TierCloudMiddleware)
   plug(ExTier.Client.ClockMiddleware)
   plug(ExTier.Client.ResponseMiddleware)
+
+  plug(Tesla.Middleware.Retry, max_retries: 3)
   plug(Tesla.Middleware.BaseUrl, "#{Application.fetch_env!(:ex_tier, :url)}/v1")
   plug(Tesla.Middleware.JSON)
 end
